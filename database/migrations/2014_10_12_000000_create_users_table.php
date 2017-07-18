b8fd97d793541ca9a858e0cca2bcf2eee3fef1ad
zyd2001 <zyd200101@outlook.com>
2017-07-18 19:51:03 +0800

    change the default avatar to fit the database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->boolean('isTeacher')->default(false);
-            $table->string('avatar')->default('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1498570329231&di=c9682486fc6ada2dedcd46219ad1302f&imgtype=0&src=http%3A%2F%2Fwww.qqzhi.com%2Fuploadpic%2F2014-10-08%2F041717696.jpg');
+            $table->string('avatar')->default('http://img4.imgtn.bdimg.com/it/u=763968628,2435880126&fm=26&gp=0.jpg');
             $table->text('setting')->nullable(); //json
             $table->boolean('isVerified')->default(false);
             $table->string('verifyCode', 6);
