c2759c647c168ce2486c03af956579e95b54cbda
zyd2001 <zyd200101@outlook.com>
2017-08-10 16:42:55 +0800

    ...

--- resources/assets/js/verify.js
+++ resources/assets/js/verify.js
@@ -18,12 +18,14 @@ const verify = new Vue({
                     $('#message').modal('show');
                     window.setTimeout("$('#message').modal('hide')", 2000);
                     self.time = 60;
+                    var save = self.text;
                     var interval = setInterval(function () {
                         self.text = 'waiting for ' + self.time;
                             self.time--;
                         if (self.time === 0) {
                             clearInterval(interval);
                             $('#re').removeClass('disabled');
+                            self.text = save;
                         }
                     }, 1000);
                 }).catch(function (err) {
