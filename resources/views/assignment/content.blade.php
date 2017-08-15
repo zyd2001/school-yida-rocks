5472adb369bae56423479e7933625842c29f89e9
dasheng <darmstadtiumbismuth@gmail.com>
2017-08-15 15:43:13 -0400

    Added description/content pages

--- resources/views/assignment/content.blade.php
+++ resources/views/assignment/content.blade.php
@@ -1,38 +1,50 @@
-<div class="row">
-	<div class="col-md-8" id="content">
-		<div class="card">
-			<div class="card-header">
-				<div class="col-md-4">Assignment Title: </div>
-				<div class="col-md-4">@{{ assignment.name }}</div>
-			</div>
-			<div class="card-title">
-				<div class="text-center">
-					<div class="row-center">
-						<h5>From Course: </h5>
-						<!-- How to get course from course id -->
-						<h4>@{{ assignment.course_id }}</h4>
-					</div>
-				</div>
-			</div>
-			<div class="card-text">
-				<p>@{{ assignment.content }}</p>
-			</div>
-			<button href="#" class="btn btn-primary">Complete This Assignment</button>
-	<!-- About to add if-statements for warnings on assignment due time -->
-			<div class="card-footer alert-warning">
-				<div class="row">
-					<p>Due Time: </p>
-					<h5>@{{ assignment.dueTime[0] }}</h5>
-				</div>
-			</div>
+<div>
+	<div class="card">
+		<div class="card-block">This is the content of the assignment</div>
+<!-- Multiple Choice Demo -->
+		<div class="radio">
+			<label><input type="radio" name="option">A</label>
+			<label><input type="radio" name="option">B</label>
+			<label><input type="radio" name="option">C</label>
+			<label><input type="radio" name="option">D</label>
+		</div>
+
+<!-- Writing prompt Demo -->
+		<div class="form-group">
+			<label for="comment">Writing Prompt: </label>
+			<textarea class="form-control" id="comment" rows="10"></textarea>
 		</div>
 	</div>
-	<!-- Grading information -->
-	<div class="col-md-4" id="grading">
-		<div class="card">
-			<!-- Add grades -->
-			<p class="card-text">Grade: </p>
-			<button href="#" class="btn btn-info">Contact Instructor</button>
-		</div>
+	<button id="get_description" class="btn btn-primary" href="#" data-toggle="modal" data-target="#confirm_msg">Back to Assignment Description
+	</button>
+	<div class="modal fade" id="confirm_msg" tabindex="-1" role="dialog" aria-labelledby="confirm_msgLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="confirm_msgLabel">Notice</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+        				</button>
+      			</div>
+			     <div class="modal-body">
+			       		<p>Are you sure you want to exit the assignment? Your progress will be saved temporarily.</p>
+			   	</div>
+		    	<div class="modal-footer">
+		    	<button type="button" class="btn btn-primary" data-dismiss="modal">Back to Assignment
+		    	</button>
+		    	<button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="
+		    		$('.assignment_description').slideToggle(function(){
+					$('#get_description').html($('.assignment_description').is(':visible')?'Confirm Exit':'Back to Assignment Description');
+					}); 
+					$('.assignment_content').slideToggle(function(){
+						$('#get_description').html($('.assignment_content').is(':visible')?'Confirm Exit':'Back to Assignment Description');
+					}); ">Confirm Exit
+				</button>
+				</div>
+    		</div>
+  		</div>
+	</div>
+	<div class="progress">
+		<div class="progress-bar bg-info" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">You have completed this percentage of the assignment</div>
 	</div>
 </div>
--- resources/views/assignment/main.blade.php
+++ resources/views/assignment/main.blade.php
@@ -1,12 +1,21 @@
 @extends('layouts.app')
 @section('content')
-    @include('assignment.content')
-    @include('assignment.description')
+	<div class="assignment_description">
+		@include('assignment.description')
+	</div>
+	<div class="assignment_content" style="display:none">
+    	@include('assignment.content')
+	</div>
 @endsection
 
 @section('script')
     @parent
     <script src="{{ asset('js/assignment.js') }}"></script>
+    <script type="text/javascript">
+    window.onbeforeunload = function(){
+  		return 'Are you sure you want to leave?';
+	};
+	</script>
 @endsection
 
 @section('head')
