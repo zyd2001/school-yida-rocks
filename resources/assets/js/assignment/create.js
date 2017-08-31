53ab19cb8721ac0d21d2396f482259548a2e6025
dasheng <darmstadtiumbismuth@gmail.com>
2017-08-31 20:56:45 +0800

    adding questions

--- resources/assets/js/assignment/create.js
+++ resources/assets/js/assignment/create.js
@@ -1,8 +1,8 @@
 if (window.innerWidth <= 800)
     showMessage('Please use PC or tablet', 0);
 
-var editor = new wangEditor('#editor_test');
-editor.create();
+// var editor = new wangEditor('#editor_test');
+// editor.create();
 
 const create = new Vue({
     el: '#assignment_create',
@@ -16,6 +16,18 @@ const create = new Vue({
     methods: {
     	createNewQuestion: function() {
     		console.log(this.num_of_questions);
-    	}
+    		this.question_type = $("#select_question_type").val();
+    		switch (this.question_type) {
+    			case 0:
+    				$("#all_questions").append("    <div class='multiple_choice mb-5 card'><h5 class='card-header col-md-12'>{{ __('message.multipleChoiceQuestion') }}</h5><div class='card-body'><textarea rows='5' class='form-control col-md-12' style='display: none'></textarea><textarea rows='5' class='form-control col-md-12' placeholder='{{ __('message.multipleChoiceCreate') }}'></textarea><div class='row'><p class='col-md-10 text-center'>{{ __('message.choices') }}</p><p class='col-md-2 text-center'>{{ __('message.correct') }}</p></div><div class='row'><input placeholder='{{ __('message.choice') }}1' class='form-control col-md-10 mb-3 mr-4 ml-3'><label class='custom-control custom-checkbox ml-5'><input type='checkbox' class='custom-control-input col-md-2'><span class='custom-control-indicator'></span><span class='custom-control-description'></span></label><!-- <input type='checkbox' class='form-control col-md-2'> --></div><div class='row'><input placeholder='{{ __('message.choice') }}2' class='form-control col-md-10 mb-3 mr-4 ml-3'><label class='custom-control custom-checkbox ml-5'><input type='checkbox' class='custom-control-input col-md-2'><span class='custom-control-indicator'></span><span class='custom-control-description'></span></label><!-- <input type='checkbox' class='form-control col-md-2'> --></div><div class='row'><input placeholder='{{ __('message.choice') }}3' class='form-control col-md-10 mb-3 mr-4 ml-3'><label class='custom-control custom-checkbox ml-5'><input type='checkbox' class='custom-control-input col-md-2'<span class='custom-control-indicator'></span<span class='custom-control-description'></span</label<!-- <input type='checkbox' class='form-control col-md-2'> --</div<div class='row'><input placeholder='{{ __('message.choice') }}4' class='form-control col-md-10 mb-3 mr-4 ml-3'><label class='custom-control custom-checkbox ml-5'><input type='checkbox' class='custom-control-input col-md-2'><span class='custom-control-indicator'></span><span class='custom-control-description'></span></label><!-- <input type='checkbox' class='form-control col-md-2'> --></div></div></div>")
+    				break;
+    			case 1:
+    				break;
+    			case 2:
+    				break;
+    			case 3:
+    				break;
+    		}
+    		console.log(this.question_type);}
     },
 });
--- resources/views/assignment/create.blade.php
+++ resources/views/assignment/create.blade.php
@@ -2,9 +2,11 @@
 @section('assignment')
 <div id="assignment_create">
     <div id="editor_test" class="mb-3"></div>
+    <div id="all_questions"></div>
     <div class="multiple_choice mb-5 card">
         <h5 class="card-header col-md-12">{{ __('message.multipleChoiceQuestion') }}</h5>
         <div class="card-body">
+            <textarea rows="5" class="form-control col-md-12" style="display: none"></textarea>
             <textarea rows="5" class="form-control col-md-12" placeholder="{{ __('message.multipleChoiceCreate') }}"></textarea>
             <div class="row">
                 <p class="col-md-10 text-center">{{ __('message.choices') }}</p>
@@ -51,6 +53,7 @@
     <div class="fill_in_the_blank mb-5 card">
         <h5 class="card-header col-md-12">{{ __('message.fitbQuestion') }}</h5>
         <div class="card-body">
+            <textarea rows="5" class="form-control col-md-12" style="display: none"></textarea>
             <div class="row">
                 <p class="col-md-7 text-center">{{ __('message.prompt') }}</p>
                 <p class="col-md-4 text-center">{{ __('message.answer') }}</p>
@@ -64,6 +67,7 @@
     <div class="matching mb-5 card">
         <h5 class="mb-3 card-header col-md-12">{{ __('message.matchingQuestions') }}</h5>
         <div class="card-body">
+            <textarea rows="5" class="form-control col-md-12" style="display: none"></textarea>
             <div class="form-group-vertical">
                 <div class="row">
                     <input class="form-control col-md-5 mb-3 ml-5 mr-4" placeholder="{{ __('message.leftChoice')}}">
@@ -91,13 +95,14 @@
     <div class="essay mb-5 card">
         <h5 class="mb-3 card-header col-md-12">{{ __('message.essayQuestion') }}</h5>
         <div class="card-body">
+            <textarea rows="5" class="form-control col-md-12" style="display: none"></textarea>
             <textarea rows="6" class="form-control text-left col-md-12" placeholder="{{ __('message.essayPrompt') }}"></textarea>      
-        </div>          
+        </div>
     </div>
 
     <div class="row">
         <label for="select_question_type">{{ __('message.selectQuestionType') }}&nbsp;</label>
-        <select class="custom-select">
+        <select class="custom-select" id="select_question_type">
             <option value="0">{{ __('message.multipleChoiceQuestion')}}</option>
             <option value="1">{{ __('message.fitbQuestion')}}</option>
             <option value="2">{{ __('message.matchingQuestions')}}</option>
