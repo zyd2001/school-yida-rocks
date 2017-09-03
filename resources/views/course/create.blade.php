a6a20dc1610030f3f401fab48d38c7209f6201dd
dasheng <darmstadtiumbismuth@gmail.com>
2017-09-03 14:43:55 +0800

    radio buttons

--- resources/views/course/create.blade.php
+++ resources/views/course/create.blade.php
@@ -7,9 +7,9 @@
        		<h5 class="card-header">{{ __('message.createCourse') }}</h5>
         	<div class="card-body">
         		<div class="row mb-3">
-			        <input placeholder="{{ __('message.nameOfCourse') }}" class="form-control col-md-8 ml-3" required>
+			        <input name="name" placeholder="{{ __('message.nameOfCourse') }}" class="form-control col-md-8 ml-3" required>
 	                <label class="custom-control custom-checkbox ml-5 mt-2">
-		                <input type="checkbox" class="custom-control-input col-md-2">
+		                <input name="public" type="checkbox" class="custom-control-input col-md-2">
 		                <span class="custom-control-indicator"></span>
 		                <span class="custom-control-description">{{ __('message.makeCoursePublic') }}</span>
 	                </label>
@@ -18,15 +18,15 @@
             	<div class="row ml-3 mt-3">
             		<span>{{ __('message.courseType') }}:</span>
 	                <label class="control control--radio ml-3 mt-1">{{ __('message.courseClass')}}
-	                    <input name="choose_Course_Type" type="radio" v-bind:value="0"/>
+	                    <input name="type" type="radio" value="0"/>
 	                    <div class="control__indicator"></div>
 	                </label>
 	                <label class="control control--radio ml-5 mt-1">{{ __('message.courseCourse')}}
-	                    <input name="choose_Course_Type" checked="true" type="radio" v-bind:value="1" />
+	                    <input name="type" checked="true" type="radio" value="1" />
 	                    <div class="control__indicator"></div>
 	                </label>
 	                <label class="control control--radio ml-5 mt-1">{{ __('message.courseGroup')}}
-	                    <input name="choose_Course_Type" type="radio" v-bind:value="2"/>
+	                    <input name="type" type="radio" value="2"/>
 	                    <div class="control__indicator"></div>
 	                </label>                
                 </div>
